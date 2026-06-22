b0aa43763a04983a79c06ca7012d5dc0473f65dd
Aniketh Pranay <anikethp@vt.edu>
2026-06-22 19:07:57 -0400

    point frontend to railway backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-const API_BASE = 'http://localhost:8000'
+const API_BASE = 'https://devpulse-production-fb8a.up.railway.app'
 
 function App() {
   const [username, setUsername] = useState('')
